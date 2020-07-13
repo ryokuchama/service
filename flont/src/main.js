5ab03406a6c5e9448c276b6d63291bd35db83b6e
tarou <r.madrid.pg@gmail.com>
2020-07-13 23:47:08 +0900

    0713

--- flont/src/main.js
+++ flont/src/main.js
@@ -5,7 +5,33 @@ import router from './router/router';
 
 Vue.config.productionTip = false
 
+var store = {
+  debug: true,
+  state: {
+
+  },
+  containsOfCart(newValue) {
+
+  },
+  timeAndPhoneNumber(newTime) {
+
+  }
+}
+
 new Vue({
+  data() {
+    return {
+      privateState: store.state
+    }
+  },
+  methods: {
+    containsOfCart: function(newValue) {
+
+    },
+    timeAndPhoneNumber: function(newTime) {
+      
+    }
+  },
   router,
   vuetify,
   render: h => h(App)
