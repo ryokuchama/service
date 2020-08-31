f266587273f6747809b0f618d8663e5040ceba84
tarou <r.madrid.pg@gmail.com>
2020-08-31 23:19:44 +0900

    0831

--- flont/src/main.js
+++ flont/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import {ValidationProvider} from 'vee-validate/dist/vee-validate.full';
+import {ValidationObserver} from 'vee-validate';
 import router from './router/router';
 import store from './store/store';
 import {sync} from 'vuex-router-sync'
@@ -8,6 +10,8 @@ import axios from 'axios';
 
 Vue.config.productionTip = false
 Vue.use(axios);
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 sync(store, router)
 
