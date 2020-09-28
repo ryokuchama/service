a79254234a575e97a8a4e8a5fd15aa0f5a9bdf2b
tarou <r.madrid.pg@gmail.com>
2020-09-29 00:32:33 +0900

    0929

--- flont/src/store/store.js
+++ flont/src/store/store.js
@@ -1,12 +1,25 @@
 import Vue from 'vue';
 import vuex from 'vuex';
+import axios from 'axios';
 
 import counter from '../store/counter';
 
 Vue.use(vuex);
 
+interface State {
+    conputedCart: [];
+    totalPrice: 0;
+    hours: 0;
+    minites: 0;
+    phoneNumber: String;
+    note: '';
+}
 export default new vuex.Store({
     modules: {
         counter
+    },
+
+    state: {
+        
     }
 });
